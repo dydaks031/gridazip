3564ec9802c29f2145d89667f15a60cd6b026a81
hyungwon.kang <hyungwon.kang@gridazip.com>
2018-02-22 18:20:19 +0900

    Add: eslint settings files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": "standard",
+  "parserOptions": {
+    "ecmaVersion": 5,
+    "sourceType": "module",
+  },
+  "globals": {
+    "$": true,
+    "swal": true,
+    "loading": true,
+    "http": true,
+    "location": true,
+    "Pagination": true,
+    "Filter": true,
+    "window": true,
+    "confirm": true,
+    "moment": true,
+    "daum": true,
+  }
+};
