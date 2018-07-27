9f80b5efed58e11fa78b1da1a174ed839e162392
ohw <ohw@gridazip.com>
2018-07-27 15:16:18 +0900

    domain change. com -> co.kr redirection

--- middlewares/domainChanger.js
+++ middlewares/domainChanger.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return function changeToCoKr(req, res, next) {
     if (req.headers.host.indexOf('gridazip.com') > -1 ) {
-      return res.redirect(301, `${req.protocol}://${req.headers.host.replace('gridazip.com', 'gridazip.co.kr')}/${req.url}`);
+      return res.redirect(301, `${req.protocol}://${req.headers.host.replace('gridazip.com', 'gridazip.co.kr')}${req.url}`);
     }
     else {
       next();
